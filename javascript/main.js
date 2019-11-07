9b353aad1ecbf0906aa57054e5bb271e6a632001
iths-stefan-sulejic <54362267+iths-stefan-sulejic@users.noreply.github.com>
2019-11-07 15:22:11 +0100

    Working API
    1. Api fungerar nu som den ska på min webbsidan.
    2. Sökfält, enter och knappen Fetch beter sig som planerat nu.

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,14 @@
 let breedData = ["Chow", "Dalmatian", "Husky"]
 breedData = breedData.map(breed => breed.toLowerCase())
-
 let ul = document.querySelector(".matches")
+let currentBreed;
 
 function matchingBreeds(userInput){
     let result = []
 
     if(userInput != ""){
         for(let breed of breedData){
-            if(breed.includes(userInput)){
+            if(breed.startsWith(userInput)){
                 result.push(breed)
             }
         }
@@ -16,7 +16,7 @@ function matchingBreeds(userInput){
     return result
 }
 
-function createList(list){
+function createDropdown(list){
     for(let breed of list){
         let li = document.createElement("li")
         li.innerText = breed
@@ -24,26 +24,48 @@ function createList(list){
     }
 }
 
-function clearList(){
+function clearDropdown(){
     let matchingBreeds = document.querySelectorAll("body > ul > li")
     for(let li of matchingBreeds){
         li.remove()
     }
 }
 
+async function getData(dogBreed){
+    let link = await fetch('https://dog.ceo/api/breed/' + dogBreed + '/images/random')
+    let convert = await link.json()
+    return convert.message
+}
+
 let input = document.querySelector("input")
 input.addEventListener("keyup", function(){
-    clearList()
+    clearDropdown()
     let matching = matchingBreeds(input.value)
-    createList(matching)
+    createDropdown(matching)
 })
 
-input.addEventListener("keyup", function(event){
+// let sup = false
+input.addEventListener("keyup", async function(event){
     if(event.keyCode === 13){
-        let button = document.createElement("button")
-        button.innerText = "Fetch"
-        document.body.append(button)
-        let image = document.createElement("img")
-        document.body.append(image)
+        let container = document.querySelector(".puppy-container")
+        let dogBreed = document.querySelector("body > ul > li:nth-child(1)").innerText
+        if(container.classList.contains("hidden")){
+            let imageContainer = document.querySelector(".hidden")
+            imageContainer.classList.remove("hidden")
+
+            let result = await getData(dogBreed)
+            document.getElementById('image').style.backgroundImage = "url('"+[result]+"')"
+
+        }else{
+            let result = await getData(dogBreed)
+            document.getElementById('image').style.backgroundImage = "url('"+[result]+"')"
+        }
+        currentBreed = dogBreed
     }
+})
+
+let button = document.querySelector("button")
+button.addEventListener("click", async function(){
+    let result = await getData(currentBreed)
+    document.getElementById('image').style.backgroundImage = "url('"+[result]+"')"
 })
